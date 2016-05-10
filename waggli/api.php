3f1ec24d92ef2d677372b2ae2dba23cb49ef773a
joepegler <joepegler123@gmail.com>
2016-05-11 00:01:05 +0100

    3

--- waggli/api.php
+++ waggli/api.php
@@ -1,11 +1,18 @@
 <?php
 	global $wpdb;
-	if (isset($_POST['id'])){
-		echo 'weeeee';
-	}
-	else{
-		$referrals = $wpdb->get_results("SELECT * FROM wp_aff_referrals");
-		// echo '<br/><br/><br/><br/><br/><br/><br/>';
-		// print_r($referrals);
+	if (isset($_POST['referral_id'])){
+		$wpdb->insert( 
+			'wp_aff_referrals', 
+			array( 
+				'referral_id' => $_POST['referral_id'], 
+				'affiliate_id' => 0,
+				datetime => 'NOW()'
+			), 
+			array( 
+				'%s', 
+				'%d',
+				'%s' 
+			) 
+		);
 	}
 ?>
